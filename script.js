e755a32336b46489c60f0769bde002b18d1c7bf2
dr1co <dricomatoba@hotmail.com>
2022-04-17 21:24:44 -0300

    Carregamento das mensagens e entrada da sala

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
-console.log("Olá, mundo!")
+let user
+
+function getUser() {
+    user = prompt("Qual é o seu nome de usuário?");
+    const request = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', {
+        name: user
+    });
+    request.then(setInterval(getChat, 3000));
+    request.catch(errorTreatment);
+    setInterval(function() {
+        axios.post('https://mock-api.driven.com.br/api/v6/uol/status', {
+            name: user
+        })
+    }, 5000)
+}
+
+function errorTreatment() {
+        alert("Usuário já conectado:");
+        getUser();
+}
+
+function getChat() {
+    console.log("carregando chat");
+    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    promise.then(loadChat);
+}
+
+function loadChat(element) {
+    const chat = document.querySelector(".chat-log");
+    chat.innerHTML = "";
+    for(let i = 0 ; i < element.data.length ; i++ )
+    {
+        switch(element.data[i].type)
+        {
+            case("status"):
+                chat.innerHTML += `<li class="message status"> 
+                    <p> <strong class="timestamp"> (${element.data[i].time}) </strong> <strong> ${element.data[i].from} </strong> ${element.data[i].text} </p>
+                </li>`;
+                break;
+            case("message"):
+                chat.innerHTML += `<li class="message regular">
+                    <p> <strong class="timestamp"> (${element.data[i].time}) </strong> <strong> ${element.data[i].from} </strong> para <strong> ${element.data[i].to}</strong>: ${element.data[i].text} </p>
+                </li>`;
+                break;
+            case("private_message"):
+                if(element.data[i].to === user || element.data[i].from === user)
+                {
+                    chat.innerHTML += `<li class="message reserved">
+                        <p> <strong class="timestamp"> (${element.data[i].time}) </strong> <strong> ${element.data[i].from} </strong> reservadamente para <strong> ${element.data[i].to}</strong>: ${element.data[i].text} </p>
+                    </li>`;
+                }
+                break;
+            default:
+                console.log("esse tipo existe não man");
+        }
+    }
+    chat.lastChild.scrollIntoView()
+}
+
+getUser();
