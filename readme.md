ebee7a6f863184176070a1d38f3e217d63fe5b84
dr1co <dricomatoba@hotmail.com>
2024-05-03 16:34:21 -0300

    [feat.] added readme.md + adjust identation

---
+++ readme.md
@@ -0,0 +1,39 @@
+# Batepapo UOL - Front-end
+
+Para ler em português, [clique aqui](#ptbr)
+
+## Description (EN)
+
+_**Note**: The API used in this project was one developed by the professors at Driven and, unfortunately, was taken down. I have no plans in deploying a new API just for this project at the moment, but will write an update if I change my mind._
+
+### Introduction
+
+Batepapo UOL was one of the oldest online messengers ever created, and this project is an attempt to recreate it for studying purposes. The project is rather simple, with a straightforward connection to an API and simple usage.
+
+### Dependencies and features
+
+In terms of dependencies, there aren't any. \o/
+
+The project features the first time I've ever used the `axios` library, to connect to a deployed API. Other than that, this is a regular <span style="color: #c90e0e">HTML</span>/<span style="color: #007bff">CSS</span>/<span style="color: #ffe500">JS</span> project
+
+### Usage
+
+The flow is simple: the user can log in the conversation by entering his username. Note that the username can't be in use, or else the user can't log in. Once he's in chat, he can type and send a message to everyone, as a public message. By clicking on the menu at the top-right corner, the user can change to whom is his next messages going to appear. That's basically it! The system also gives a message to the chat announcing when an user entered or left the chat.
+
+## <a name="ptbr" /> Descrição (PT-BR)
+
+_**Nota**: A API usada neste projeto foi desenvolvida pelos professores da Driven e, infelizmente, foi desativada. Não tenho planos de implantar uma nova API apenas para este projeto no momento, mas escreverei uma atualização se mudar de ideia._
+
+### Introdução
+
+O Batepapo UOL foi um dos mais antigos mensageiros online já criados, e este projeto é uma tentativa de recriá-lo para fins de estudo. O projeto é bastante simples, com uma conexão direta a uma API e uso simples.
+
+### Dependências e recursos
+
+Em termos de dependências, não há nenhuma. \o/
+
+O projeto apresenta a primeira vez que usei a biblioteca `axios`, para conectar a uma API implantada. Fora isso, este é um projeto regular de <span style="color: #c90e0e">HTML</span>/<span style="color: #007bff">CSS</span>/<span style="color: #ffe500">JS</span>.
+
+### Uso
+
+O funcionamento é simples: o usuário pode entrar na conversa digitando seu nome de usuário. Observe que o nome de usuário não pode estar em uso, caso contrário, o usuário não poderá entrar. Uma vez na conversa, ele pode digitar e enviar uma mensagem para todos, como uma mensagem pública. Ao clicar no menu no canto superior direito, o usuário pode escolher para quem suas próximas mensagens serão direcionadas. Basicamente é isso! O sistema também envia uma mensagem para o chat anunciando quando um usuário entra ou sai do chat.
--- script.js
+++ script.js
@@ -1,184 +1,187 @@
 let user;
-let receiver = "Todos"
-let messageType = "message"
-const messageButton = document.querySelector(".message-text").querySelector("input")
-const sendUserButton = document.querySelector(".login-screen").querySelector("input")
-const apiBaseUrl = 'https://mock-api.driven.com.br/api/v6/uol'
+let receiver = "Todos";
+let messageType = "message";
+const messageButton = document
+  .querySelector(".message-text")
+  .querySelector("input");
+const sendUserButton = document
+  .querySelector(".login-screen")
+  .querySelector("input");
+const apiBaseUrl = "https://mock-api.driven.com.br/api/v6/uol";
 
 function getUser() {
-    user = document.querySelector(".login-screen").querySelector("input").value;
-    if(user === "")
-    {
-        alert("Nome de usuário vazio: tente novamente");
-        return;
-    }
-    const request = axios.post(`${apiBaseUrl}/participants`, {
-        name: user
-    });
-    request.then(function() {
-        document.querySelector(".login-screen").style.display = "none";
-    });
-    request.then(getChat);
-    request.then(setInterval(getChat, 3000));
-    request.then(getParticipants);
-    request.then(setInterval(getParticipants, 10000));
-    request.then(setInterval(function() {
-        axios.post(`${apiBaseUrl}/status`, {
-            name: user
-        })
-    }, 5000));
-    request.catch(function() {
-        alert("Usuário já conectado:");
-        sendUserButton.addEventListener("keyup", clickUserButton);
-    });
+  user = document.querySelector(".login-screen").querySelector("input").value;
+  if (user === "") {
+    alert("Nome de usuário vazio: tente novamente");
+    return;
+  }
+  const request = axios.post(`${apiBaseUrl}/participants`, {
+    name: user,
+  });
+  request.then(function () {
+    document.querySelector(".login-screen").style.display = "none";
+  });
+  request.then(getChat);
+  request.then(setInterval(getChat, 3000));
+  request.then(getParticipants);
+  request.then(setInterval(getParticipants, 10000));
+  request.then(
+    setInterval(function () {
+      axios.post(`${apiBaseUrl}/status`, {
+        name: user,
+      });
+    }, 5000)
+  );
+  request.catch(function () {
+    alert("Usuário já conectado:");
+    sendUserButton.addEventListener("keyup", clickUserButton);
+  });
 }
 
 function getChat() {
-    const promise = axios.get(`${apiBaseUrl}/messages`);
-    promise.then(loadChat);
+  const promise = axios.get(`${apiBaseUrl}/messages`);
+  promise.then(loadChat);
 }
 
 function loadChat(element) {
-    const chat = document.querySelector(".chat-log");
-    chat.innerHTML = "";
-    for(let i = 0 ; i < element.data.length ; i++)
-    {
-        switch(element.data[i].type)
-        {
-            case("status"):
-                chat.innerHTML += `<li class="message status"> 
+  const chat = document.querySelector(".chat-log");
+  chat.innerHTML = "";
+  for (let i = 0; i < element.data.length; i++) {
+    switch (element.data[i].type) {
+      case "status":
+        chat.innerHTML += `<li class="message status"> 
                     <p> <strong class="timestamp"> (${element.data[i].time}) </strong> <strong> ${element.data[i].from} </strong> ${element.data[i].text} </p>
                 </li>`;
-                break;
-            case("message"):
-                chat.innerHTML += `<li class="message regular">
+        break;
+      case "message":
+        chat.innerHTML += `<li class="message regular">
                     <p> <strong class="timestamp"> (${element.data[i].time}) </strong> <strong> ${element.data[i].from} </strong> para <strong> ${element.data[i].to}</strong>: ${element.data[i].text} </p>
                 </li>`;
-                break;
-            case("private_message"):
-                if(element.data[i].to === user || element.data[i].from === user)
-                {
-                    chat.innerHTML += `<li class="message reserved">
+        break;
+      case "private_message":
+        if (element.data[i].to === user || element.data[i].from === user) {
+          chat.innerHTML += `<li class="message reserved">
                         <p> <strong class="timestamp"> (${element.data[i].time}) </strong> <strong> ${element.data[i].from} </strong> reservadamente para <strong> ${element.data[i].to}</strong>: ${element.data[i].text} </p>
                     </li>`;
-                }
-                break;
-            default:
-                console.log(`Esse tipo da mensagem ${i} não existe`);
         }
+        break;
+      default:
+        console.log(`Esse tipo da mensagem ${i} não existe`);
     }
-    chat.lastChild.scrollIntoView();
+  }
+  chat.lastChild.scrollIntoView();
 }
 
 function getParticipants() {
-    const promise = axios.get(`${apiBaseUrl}/participants`);
-    promise.then(loadParticipants);
+  const promise = axios.get(`${apiBaseUrl}/participants`);
+  promise.then(loadParticipants);
 }
 
 function loadParticipants(element) {
-    const participants = document.querySelector(".users-list");
-    participants.innerHTML = `<li class="user selected" data-identifier="participant" onclick="selectUser(this)">
+  const participants = document.querySelector(".users-list");
+  participants.innerHTML = `<li class="user selected" data-identifier="participant" onclick="selectUser(this)">
         <ion-icon class="user-icon" name="people"></ion-icon>
         <p>Todos</p>
         <ion-icon class="check-icon show" name="checkmark-sharp"></ion-icon>
     </li>`;
-    for(let i = 0 ; i < element.data.length ; i++)
-    {
-        participants.innerHTML += `<li class="user" onclick="selectUser(this)">
+  for (let i = 0; i < element.data.length; i++) {
+    participants.innerHTML += `<li class="user" onclick="selectUser(this)">
             <ion-icon class="user-icon" name="person-circle"></ion-icon>
             <p>${element.data[i].name}</p>
             <ion-icon class="check-icon" name="checkmark-sharp"></ion-icon>
-        </li>`
-    }
+        </li>`;
+  }
 }
 
 function toggleSidebar() {
-    const background = document.querySelector(".background");
-    const sidebar = document.querySelector(".sidebar");
-    background.classList.toggle("show");
-    sidebar.classList.toggle("show");
+  const background = document.querySelector(".background");
+  const sidebar = document.querySelector(".sidebar");
+  background.classList.toggle("show");
+  sidebar.classList.toggle("show");
 }
 
 function selectUser(element) {
-    const messageBox = document.querySelector(".message-text");
-    const selected = document.querySelector(".users-list").querySelector(".selected");
-    const checkSelected = document.querySelector(".users-list").querySelector(".show");
-    if(selected !== null)
-    {
-        selected.classList.remove("selected");
-        checkSelected.classList.remove("show");
-    }
-    element.classList.add("selected");
-    element.querySelector(".check-icon").classList.add("show");
-    receiver = element.querySelector("p").innerHTML;
-    if(messageType === "message")
-    {
-        messageBox.innerHTML = `<input type="text" placeholder="Escreva aqui...">`;
-    }
-    if(messageType === "private_message")
-    {
-        messageBox.innerHTML = `<input type="text" placeholder="Escreva aqui...">
+  const messageBox = document.querySelector(".message-text");
+  const selected = document
+    .querySelector(".users-list")
+    .querySelector(".selected");
+  const checkSelected = document
+    .querySelector(".users-list")
+    .querySelector(".show");
+  if (selected !== null) {
+    selected.classList.remove("selected");
+    checkSelected.classList.remove("show");
+  }
+  element.classList.add("selected");
+  element.querySelector(".check-icon").classList.add("show");
+  receiver = element.querySelector("p").innerHTML;
+  if (messageType === "message") {
+    messageBox.innerHTML = `<input type="text" placeholder="Escreva aqui...">`;
+  }
+  if (messageType === "private_message") {
+    messageBox.innerHTML = `<input type="text" placeholder="Escreva aqui...">
         <p> Enviando para ${receiver} (reservadamente) </p>`;
-    }
+  }
 }
 
 function selectType(element) {
-    const messageBox = document.querySelector(".message-text");
-    const selected = document.querySelector(".type-list").querySelector(".selected");
-    const checkSelected = document.querySelector(".type-list").querySelector(".show");
-    if(selected !== null)
-    {
-        selected.classList.remove("selected");
-        checkSelected.classList.remove("show");
-    }
-    element.classList.add("selected");
-    element.querySelector(".check-icon").classList.add("show");
-    if(element.querySelector("p").innerHTML === "Público")
-    {
-        messageBox.innerHTML = `<input type="text" placeholder="Escreva aqui...">`;
-        messageType = "message";
-    }
-    if(element.querySelector("p").innerHTML === "Reservadamente")
-    {
-        messageBox.innerHTML = `<input type="text" placeholder="Escreva aqui...">
+  const messageBox = document.querySelector(".message-text");
+  const selected = document
+    .querySelector(".type-list")
+    .querySelector(".selected");
+  const checkSelected = document
+    .querySelector(".type-list")
+    .querySelector(".show");
+  if (selected !== null) {
+    selected.classList.remove("selected");
+    checkSelected.classList.remove("show");
+  }
+  element.classList.add("selected");
+  element.querySelector(".check-icon").classList.add("show");
+  if (element.querySelector("p").innerHTML === "Público") {
+    messageBox.innerHTML = `<input type="text" placeholder="Escreva aqui...">`;
+    messageType = "message";
+  }
+  if (element.querySelector("p").innerHTML === "Reservadamente") {
+    messageBox.innerHTML = `<input type="text" placeholder="Escreva aqui...">
         <p> Enviando para ${receiver} (reservadamente) </p>`;
-        messageType = "private_message";
-    }
+    messageType = "private_message";
+  }
 }
 
 function sendMessage() {
-    const message = document.querySelector(".message-text").querySelector("input");
-    if(message.value === "") return;
-    const request = axios.post(`${apiBaseUrl}/messages`, {
-        from: user,
-        to: receiver,
-        text: message.value,
-        type: messageType,
-    });
-    request.then(function() {
-        getChat();
-        message.value = "";
-    });
-    request.catch(function() {
-        alert("Erro! Usuário não encontrado");
-        window.location.reload();
-    });
+  const message = document
+    .querySelector(".message-text")
+    .querySelector("input");
+  if (message.value === "") return;
+  const request = axios.post(`${apiBaseUrl}/messages`, {
+    from: user,
+    to: receiver,
+    text: message.value,
+    type: messageType,
+  });
+  request.then(function () {
+    getChat();
+    message.value = "";
+  });
+  request.catch(function () {
+    alert("Erro! Usuário não encontrado");
+    window.location.reload();
+  });
 }
 
 function clickMessageButton(event) {
-    if (event.keyCode === 13)
-    {
-        event.preventDefault();
-        document.querySelector(".message-box").querySelector("button").click();
-    }
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.querySelector(".message-box").querySelector("button").click();
+  }
 }
 
 function clickUserButton(event) {
-    if (event.keyCode === 13)
-    {
-        event.preventDefault();
-        document.querySelector(".login-screen").querySelector("button").click();
-    }
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.querySelector(".login-screen").querySelector("button").click();
+  }
 }
 
 sendUserButton.addEventListener("keyup", clickUserButton);
